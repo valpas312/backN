f65bfe2f9815cef93bdc49b9f8d2e12778ed7ec5
valpas312 <valenpalmas2005@gmail.com>
2023-06-04 21:09:20 -0300

    stdout y stdin

--- process.js
+++ process.js
@@ -1,7 +1,28 @@
+// console.log(process.argv)
 
-function leeropcion(opcion){
-    const index = process.argv.indexOf(opcion)
-    console.log(index)
-}
+// function leeropcion(opcion){
+//     const index = process.argv.indexOf(opcion)
+//     console.log(index)
+//     if(index === -1){
+//         return false
+//     }
+//     return process.argv[index + 1]
+// }
 
-console.log(leeropcion('--base'))
+// const nombre = leeropcion('--nombre')
+// console.log(`El nombre es: ${nombre}`)
+
+process.stdout.write("Cual es tu nombre?");
+
+process.stdin.on("data", function (data) {
+  process.stdout.write(`Hola ${data.toString()}`);
+
+  process.stdout.write("Cual es tu apellido?");
+
+  process.stdin.on("data", function (data) {
+    process.stdout.write(`Hola ${data.toString()}`);
+
+    process.exit();
+  });
+
+});
